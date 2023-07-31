d7584a04f4f03a0dbe7041c5840922be9276efab
sai prakash <nagraju.k675@gmail.com>
2023-07-31 16:13:53 -0500

    Deploy Rewards app to GitHub Pages

---
+++ precache-manifest.c533ce2fa1a55c451201ded9bbaeb6bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb2553a264f4bf61d081e9f4bebcb52",
+    "url": "/reward-points-webapp/index.html"
+  },
+  {
+    "revision": "534cb0bf422294f18b7e",
+    "url": "/reward-points-webapp/static/css/main.984b2baa.chunk.css"
+  },
+  {
+    "revision": "be41c4df180f5687323f",
+    "url": "/reward-points-webapp/static/js/2.4167f18c.chunk.js"
+  },
+  {
+    "revision": "534cb0bf422294f18b7e",
+    "url": "/reward-points-webapp/static/js/main.4c7b471d.chunk.js"
+  },
+  {
+    "revision": "a5f6946f3482ee755775",
+    "url": "/reward-points-webapp/static/js/runtime~main.4bb32da4.js"
+  }
+]);
